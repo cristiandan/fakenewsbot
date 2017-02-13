545ae1190836c7750bf0fde7d484b3a6aa238156
Cristian Dan <cristian.dan@live.com>
2017-02-13 14:04:40 +0200

    get user info then save to db

--- src/messageHandler.js
+++ src/messageHandler.js
@@ -25,8 +25,6 @@ module.exports.handleMessage = (event, context, callback) => {
             var pageID = entry.id;
             var timeOfEvent = entry.time;
 
-            // Iterate over each messaging event
-            console.log("entry", entry);
             entry.messaging.forEach(function (event) {
                 if (event.message) {
                     receivedMessage(event);
@@ -37,86 +35,115 @@ module.exports.handleMessage = (event, context, callback) => {
         });
     }
 
-    function receivedMessage(event) {
-        console.log('enter received event');
-        var senderID = event.sender.id;
-        var recipientID = event.recipient.id;
-        var timeOfMessage = event.timestamp;
-        var message = event.message;
-
-        const user = new User({id: senderID});
-        user.save(function(err){
-            if (err) {
-                console.log({info: 'error during cat create', error: err});
-            };
-            console.log({info: 'cat created successfully'});
-        });
-
-        console.log("Received message for user %d and page %d at %d with message:",
-            senderID, recipientID, timeOfMessage);
-        console.log(JSON.stringify(message));
-
-        var messageId = message.mid;
-
-        var messageText = message.text;
-        var messageAttachments = message.attachments;
-
-        if (messageText) {
-
-            // If we receive a text message, check to see if it matches a keyword
-            // and send back the example. Otherwise, just echo the text we received.
-            switch (messageText) {
-                case 'generic':
-                    sendTextMessage(senderID, "messageText12");
-                    break;
-
-                default:
-                    sendTextMessage(senderID, messageText);
-            }
-        } else if (messageAttachments) {
-            sendTextMessage(senderID, "Message with attachment received");
-        }
-    }
-
-    function sendTextMessage(recipientId, messageText) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                text: messageText
-            }
-        };
-
-        callSendAPI(messageData);
-    }
-
-    function callSendAPI(messageData) {
-        request({
-            uri: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: process.env.PAGE_ACCESS_TOKEN
-            },
-            method: 'POST',
-            json: messageData
-
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var recipientId = body.recipient_id;
-                var messageId = body.message_id;
-
-                console.log("Successfully sent generic message with id %s to recipient %s",
-                    messageId, recipientId);
-
-            } else {
-                console.error("Unable to send message.");
-                console.error(response);
-                console.error(error);
-            }
-            
-        });
-    }
-    
     mongoose.connection.close();
     done(null, "ok");
+}
+
+
+function receivedMessage(event) {
+    var senderId = event.sender.id;
+    var recipientId = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    getUserInfo(senderId, function(userInfo){
+        console.log('UserInfo', userInfo);
+        saveUserData(senderId, recipientId, userInfo);
+    });
+
+    var messageId = message.mid;
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+        handleTextMessage(message, senderId, recipientId, timeOfMessage);
+    } else if (messageAttachments) {
+        handleMsgWithAttachament(message);
+    }
+}
+
+function handleMsgWithAttachament(message) {
+    sendTextMessage(senderId, "Message with attachment received");    
+}
+
+function handleTextMessage(message, senderId, recipientId, timeOfMessage) {
+
+    var messageText = message.text;
+
+    switch (messageText) {
+            case 'generic':
+                sendTextMessage(senderId, "messageText12");
+                break;
+
+            default:
+                sendTextMessage(senderId, messageText);
+        }
+}
+
+function saveUserData(userId, pageMessaged, userData) {
+    Object.assign(userData, { userId: userId, pageMessaged: pageMessaged });
+    const user = new User(userData);
+    user.save(function (err) {
+        if (err) {};
+    
+    });
+}
+
+function getUserInfo(userId, callback) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/'+userId,
+        qs: {
+            access_token: process.env.PAGE_ACCESS_TOKEN,
+            fields: 'first_name,locale,profile_pic,last_name,timezone,gender'
+        },
+        method: 'GET'
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            callback(JSON.parse(body));
+        } else {
+            console.error("Unable to send message.");
+            console.error("response", response);
+            console.error("error", error);
+        }
+    });
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: process.env.PAGE_ACCESS_TOKEN
+        },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+
+    });
 }
--- src/userModel.js
+++ src/userModel.js
@@ -3,10 +3,13 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     first_name: String,
     last_name: String,
-    id: String,
+    userId: String,
     profile_pic: String,
     locale: String,
-    timezone: Number
+    timezone: Number,
+    gender: String,
+    pageMessaged: String,
+    timeOfFirstMessage: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
